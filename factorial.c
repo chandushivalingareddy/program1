74478e776da9d36b2a700837317f246940e1b143
chandushivalingareddy <53473880+chandushivalingareddy@users.noreply.github.com>
2019-08-26 16:27:51 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+
+{
+    
+int fact=1,n,i;
+    
+//printf("enter the num\n");
+    
+scanf("%d",&n);
+    
+for(i=n;i>1;i--)
+    
+{
+        
+fact=fact*i;
+    
+}
+    
+printf("%d",fact);
+    
+return 0;
+
+}    
---
+++ primenumber.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+
+int i,N,j;
+
+int count=0;
+
+scanf("%d",&N);
+
+for(i=2;i<N;i++)
+
+{
+
+
+
+for(j=1;j<=i;j++)
+
+{
+
+if(i%j==0)
+count++;
+
+}
+ 
+if(count==2)
+
+{
+ 
+printf("%d ",i);
+ 
+}
+
+count=0;
+
+} 
+
+
+}
